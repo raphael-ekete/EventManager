443e5ddec34a173e47e3c40f3d478f6d8630381c
Raphael Ekete <raphael.ekete@gmail.com>
2017-11-19 13:01:57 +0100

    first raph-work-folder commit, add features

--- template/scripts/uiscript.js
+++ template/scripts/uiscript.js
@@ -2,3 +2,7 @@ $(document).ready(function() {
     $('input#input_text, textarea#textarea1').characterCounter();
     $('select').material_select();
   });
+
+$(document).ready(function(){
+      $('.slider').slider();
+    });
